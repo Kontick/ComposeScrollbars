58a42efa4fa920e1cc8d47b51d625dd4eaa58ad5
ivantitov <ivan@ahoikapptn.com>
2025-08-28 12:18:25 +0200

    Added KotlinDoc to scrollbarStickyHeader function.

--- ComposeScrollbars/src/main/kotlin/com/gigamole/composescrollbars/ScrollbarStickyHeader.kt
+++ ComposeScrollbars/src/main/kotlin/com/gigamole/composescrollbars/ScrollbarStickyHeader.kt
@@ -7,7 +7,13 @@ import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
 
-fun LazyListScope.scrollBarStickyHeader(
+/**
+ * Adds a sticky header to a [LazyListScope] that can be detected by [ScrollbarsState] to ensure proper scroll behavior.
+ *
+ * @param contentType Optional type of the content, used for optimization in Lazy lists.
+ * @param content The composable content of the sticky header.
+ */
+fun LazyListScope.scrollbarStickyHeader(
     contentType: Any? = null,
     content: @Composable LazyItemScope.() -> Unit
 ) {
